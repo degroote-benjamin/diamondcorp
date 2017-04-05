c3fd3d21bce725d1c8c05cec10417b059918cfa9
degroote.benjamin <degroote.benjamin@gmail.com>
2017-04-05 15:45:29 +0200

    add nav for mobile , tablet and pc in index.html

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+  // Define the height of your hero here
+  var heroHeight = 1000;
+
+  $(window).scroll(function() {
+    if ($(document).scrollTop() < heroHeight) {
+
+      $("#menu").css({
+        "position": "absolute",
+        "top": heroHeight
+      });
+    } else {
+      $("#menu").css({
+        "position": "fixed",
+        "top": "0"
+      });
+    }
+  });
+});
+
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
